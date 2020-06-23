0b55101204cdb66bb511dd01c15ec2d6f46c0f49
Evgeny Stepanov <evgenos@list.ru>
2020-06-23 16:19:11 +0300

    added section with creators and switch actors/creators

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ document.onclick = (e) => {
     menu.classList.toggle('active');
   }
 };
+
+document.querySelector('.people__titles').onclick = function (e) {
+  let target = e.target;
+  if (target.classList.contains('people__title')) {
+    let activeTitle = document.querySelector('.people__title-active');
+    let people__block = document.querySelectorAll('.people__block');
+    if (!target.classList.contains('.people__title-active')) {
+      activeTitle.classList.remove('people__title-active');
+      target.classList.add('people__title-active');
+      for (let i of people__block) {
+        i.classList.toggle('active');
+      }
+    }
+  }
+};
